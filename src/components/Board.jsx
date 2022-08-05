121cd2ac36d92d85386e31f3bddf4c32b16a03ad
Fexol <fexol25@gmail.com>
2022-08-05 11:32:19 +0200

    created board and square components

---
+++ src/components/Board.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Sqare from './Sqare';
+
+const Board = () => {
+  return (
+    <div>
+      <div>
+        <Sqare value={0} />
+        <Sqare value={1} />
+        <Sqare value={2} />
+      </div>
+      <div>
+        <Sqare value={3} />
+        <Sqare value={4} />
+        <Sqare value={5} />
+      </div>
+      <div>
+        <Sqare value={6} />
+        <Sqare value={7} />
+        <Sqare value={8} />
+      </div>
+    </div>
+  );
+};
+
+export default Board;
